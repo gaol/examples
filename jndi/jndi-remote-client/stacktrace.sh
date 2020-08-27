5bdbc6c8cecfef61b9ad8ad8c6a16c92e0023bff
Lin Gao <aoingl@gmail.com>
2020-08-27 11:06:15 +0800

    byteman script for jndi

---
+++ jndi/jndi-remote-client/stacktrace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+target="$1"
+
+bmfile="src/main/resources/$target.btm"
+
+[ ! -e "$bmfile" ] && echo -e "$bmfile does not exist!" && exit 1
+
+cmd="bmjava -l $bmfile"
+
+if [ -z "${target##*jndi*}" ] ;then
+  cmd="$cmd -Djndi=true "
+fi
+
+jarfile=`ls target/|grep with-dependencies`
+cmd="$cmd -jar target/$jarfile"
+echo -e "$cmd"
+$cmd
+
